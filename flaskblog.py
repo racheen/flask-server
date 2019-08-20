e995d6b2ea00b08dbe4f2feb5ca320b03cbe97bc
Rachel Tumulak <rmktumulak@gmail.com>
2019-08-20 17:29:53 +0800

    first init

---
+++ flaskblog.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY']="u'a124ad2c6e71c86be14421df74761051"
+
+posts = [
+    {
+        'author': 'Racheen',
+        'title': "Blog Post 1",
+        'content': "First post content",
+        'date_posted': "April 1, 2019"
+    },
+    {
+        'author': 'Racheen',
+        'title': "Blog Post 2",
+        'content': "Second post content",
+        'date_posted': "April 1, 2019"
+    }
+]
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['POST','GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}!'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
