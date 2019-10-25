6070ddfd3bf7cd99d1c07e974c2efac06c21f312
Rachel Tumulak <rmktumulak@gmail.com>
2019-10-25 14:38:24 +0800

    Modify config to boto library

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,19 +1,23 @@
 import os
+from boto.s3.connection import S3Connection as conf
 
 class Config:
-    # SECRET_KEY="u'a124ad2c6e71c86be14421df74761051"
-    # DATABASE_URI='postgres://postgres:admin@localhost:5432/flask-server38'
-    # MAIL_SERVER = 'smtp.mailtrap.io'
-    # MAIL_PORT = 2525
-    # MAIL_USE_TLS = True
-    # MAIL_USE_SSL = False
-    # MAIL_USERNAME = '3f118538243430'
-    # MAIL_PASSWORD = 'c9c4b0be481af8'
-    SECRET_KEY = os.environ['SECRET_KEY']
-    DATABASE_URI= os.environ['DATABASE_URI']
-    MAIL_SERVER = os.environ['MAIL_SERVER']
-    MAIL_PORT = os.environ['MAIL_PORT']
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ['MAIL_USERNAME']
-    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    dev = False
+    if dev == True:
+        SECRET_KEY = os.environ['SECRET_KEY']
+        DATABASE_URI= os.environ['DATABASE_URI']
+        MAIL_SERVER = os.environ['MAIL_SERVER']
+        MAIL_PORT = os.environ['MAIL_PORT']
+        MAIL_USE_TLS = True
+        MAIL_USE_SSL = False
+        MAIL_USERNAME = os.environ['MAIL_USERNAME']
+        MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    else:
+        SECRET_KEY = conf(os.environ['SECRET_KEY'])
+        DATABASE_URI= conf(os.environ['DATABASE_URI'])
+        MAIL_SERVER = conf(os.environ['MAIL_SERVER'])
+        MAIL_PORT = conf(os.environ['MAIL_PORT'])
+        MAIL_USE_TLS = True
+        MAIL_USE_SSL = False
+        MAIL_USERNAME = conf(os.environ['MAIL_USERNAME'])
+        MAIL_PASSWORD = conf(os.environ['MAIL_PASSWORD'])
