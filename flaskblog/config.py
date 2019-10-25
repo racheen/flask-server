048c12cad394bae2988d3557288a9a1367e8e83f
Rachel Tumulak <rmktumulak@gmail.com>
2019-10-25 13:46:45 +0800

    Fixed config

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -9,11 +9,11 @@ class Config:
     # MAIL_USE_SSL = False
     # MAIL_USERNAME = '3f118538243430'
     # MAIL_PASSWORD = 'c9c4b0be481af8'
-    SECRET_KEY= os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ['SECRET_KEY']
     DATABASE_URI= os.environ['DATABASE_URI']
     MAIL_SERVER = os.environ['MAIL_SERVER']
     MAIL_PORT = os.environ['MAIL_PORT']
-    MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
-    MAIL_USE_SSL = os.environ['MAIL_USE_SSL']
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ['MAIL_USERNAME']
     MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
